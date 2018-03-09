2bcb21db234e5fbdf085f289c42a8501d2fc7136
khaled amiri <khaled.amiri@sofrecom.com>
2018-03-09 09:45:22 +0100

    first commit

---
+++ src/main/java/tn/gps/vehicule/GpsApplication.java
@@ -0,0 +1,12 @@
+package tn.gps.vehicule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpsApplication.class, args);
+	}
+}
